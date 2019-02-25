83928e8c20895f590dba1b8fc677125517fb72a8
Charles Kelly <kellyc7c@live.com>
2019-02-25 09:16:06 +0000

    Refactored final score to make it easier to read

--- BowlingGameXP/BowlingGame.cs
+++ BowlingGameXP/BowlingGame.cs
@@ -23,24 +23,44 @@ namespace BowlingGameXP
 
             for (int i = 0; i < frames.Count(); i++)
             {
-                if (frames[i].IsSpare() && i < frames.Count() - 1)
-                {
-                    totalScore = frames[i+1].AddSpareBonus(totalScore);
-                }
-
-                if (frames[i].IsStrike() && i < frames.Count() - 1)
-                {
-                    if (i < frames.Count() - 2 && frames[i + 1].IsStrike())
-                    {
-                        totalScore = frames[i + 2].AddSpareBonus(totalScore);
-                    }
-                    totalScore = totalScore.Add(frames[i + 1].StrikeBonus());
-                }
+                totalScore = CalculateSpare(totalScore, frames, i);
+                totalScore = CalculateStrike(totalScore, frames, i);
 
                 totalScore = totalScore.Add(frames[i].GetIntermediateScore());
             }
 
             return totalScore.ToInt();
+        }
+
+        private static BowlScore CalculateStrike(BowlScore totalScore, Frames frames, int i)
+        {
+            if (frames[i].IsStrike() && i < frames.Count() - 1)
+            {
+                totalScore = CalculateStrikeBonusIfNotPenultimateFrame(totalScore, frames, i);
+                totalScore = totalScore.Add(frames[i + 1].StrikeBonus());
+            }
+
+            return totalScore;
+        }
+
+        private static BowlScore CalculateStrikeBonusIfNotPenultimateFrame(BowlScore totalScore, Frames frames, int i)
+        {
+            if (i < frames.Count() - 2 && frames[i + 1].IsStrike())
+            {
+                totalScore = frames[i + 2].AddSpareBonus(totalScore);
+            }
+
+            return totalScore;
+        }
+
+        private static BowlScore CalculateSpare(BowlScore totalScore, Frames frames, int i)
+        {
+            if (frames[i].IsSpare() && i < frames.Count() - 1)
+            {
+                totalScore = frames[i + 1].AddSpareBonus(totalScore);
+            }
+
+            return totalScore;
         }
     }
 }
